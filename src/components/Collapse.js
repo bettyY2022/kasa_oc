7a7a51362422ef778e461a6d3acd16250b1c7af3
jadliaissam <jadliaissam@gmail.com>
2023-01-16 22:49:00 +0100

    details page 90% done

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -6,18 +6,18 @@ import arrow_down from "../arrow_down.svg";
 export default function Collapse(props) {
   const [isOpen, setIsOpen] = useState(true);
   let arrow;
-  if (isOpen) arrow = <img src={arrow_up} alt="up" />;
-  else arrow = <img src={arrow_down} alt="down" />;
+  if (isOpen)
+    arrow = <img src={arrow_up} alt="up" style={{ marginRight: 20 }} />;
+  else arrow = <img src={arrow_down} alt="down" style={{ marginRight: 20 }} />;
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", margin: 10 }}>
-      <div className="borderDescription"
-        onClick={() => setIsOpen(!isOpen)}>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <div className="borderDescription" onClick={() => setIsOpen(!isOpen)}>
         <p> {props.title}</p>
         {arrow}
       </div>
       {isOpen && (
-        <div className="description" >
+        <div className="description">
           <p>{props.content}</p>
         </div>
       )}
--- src/pages/Details.js
+++ src/pages/Details.js
@@ -11,24 +11,78 @@ function Details() {
   const { logementID } = useParams();
   const data = logements.find((logement) => logement.id === logementID);
   return (
-    <div >
-      <img alt="logements" className="slider" src={data.cover} />
-      <p className="content">
-        <b>
-          {data.title}
-          <br></br>
-        </b>
-        {data.location}
-      </p>
-      <div style={{ display: "flex" }}>
-        {data.tags.map((tag) => (
-          <Tag key={tag} text={tag} />
-        ))}
-      </div>
-      <div >
-        <Rating value={data.rating}  />
-        <Collapse title="Description" content={data.description} />
-        <Collapse title="Équipements" content={data.equipments} />
+    <div
+      style={{
+        width: "w-100",
+        justifyContent: "center",
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ width: "85%" }}>
+        <img alt="logements" className="slider" src={data.cover} />
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <div style={{ width: "80%" }}>
+            <p className="content">
+              <p>{data.title}</p>
+              {data.location}
+            </p>
+            <div style={{ display: "flex" }}>
+              {data.tags.map((tag) => (
+                <Tag key={tag} text={tag} />
+              ))}
+            </div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-end",
+            }}
+          >
+            <div style={{ display: "flex", flexDirection: "row" }}>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  margin: 10,
+                }}
+              >
+                <span style={{ marginBottom: 10 }}>{data.host.name}</span>
+              </div>
+              <div>
+                <img
+                  src={data.host.picture}
+                  style={{ borderRadius: 75 }}
+                  width={64}
+                />
+              </div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "flex-end",
+              }}
+            >
+              <Rating value={data.rating} />
+            </div>
+          </div>
+        </div>
+        <div style={{ display: "flex", flex: 2 }}>
+          <div style={{ flex: 1, margin: 10 }}>
+            <Collapse title="Description" content={data.description} />
+          </div>
+          <div style={{ flex: 1, margin: 10 }}>
+            <Collapse
+              title="Équipements"
+              content={data.equipments.map((elt) => (
+                <p style={{ marginTop: 0, marginBottom: 0 }}>{elt}</p>
+              ))}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
